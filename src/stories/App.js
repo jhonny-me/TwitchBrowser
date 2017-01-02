e087b5fc5c15a56733c547d497f65a87cc639b53
Johnny Gu <johnny@wiredcraft.com>
2017-01-02 22:10:31 +0800

    finished with bugs

--- src/stories/App.js
+++ src/stories/App.js
@@ -7,9 +7,19 @@ import $ from 'jquery'
 require('../css/App.css')
 
 const status = ['all', 'online', 'offline']
-const baseAPI = ''
-const users = []
+const baseAPI = 'https://wind-bow.gomix.me/twitch-api/'
+const users = ['freecodecamp',
+    'storbeck','ESL_SC2', 'test_dontexists']
 const title = 'Twitch Borwser'
+
+function Item(json) {
+    return {
+        logo: json.logo,
+        name: json.display_name,
+        detail: json.stream.channel.status,
+        url: json.stream.channel.url,
+    }
+}
 
 export default class App extends Component {
 
@@ -25,7 +35,44 @@ export default class App extends Component {
     }
 
     componentDidMount() {
+        var originalDatas = []
+        const that = this
+        users.forEach(function(v){
+            const url = baseAPI + 'users/' + v
+            $.ajax({
+                url: url,
+                jsonp: "callback",
+                dataType: "jsonp",
+                success: function( json ) {
+                    console.log( json ); // server response
+                    var item = {name: v}
+                    if (json._id){
+                        item.name = json.display_name
+                        item.logo = json.logo
+                    }
 
+                    const streamUrl = baseAPI + 'streams/' + v
+                    $.ajax({
+                        url: streamUrl,
+                        dataType: 'jsonp',
+                        success: function(newJson) {
+                            console.log(newJson)
+                            if (newJson.stream) {
+                                item.detail = newJson.stream.channel.status
+                                item.url = newJson.stream.channel.url
+                                item.status = 'online'
+                            }else {
+                                item.status = 'offline'
+                            }
+                            originalDatas.push(item)
+                            if (originalDatas.length === 4) {
+                                that.setState({datas: originalDatas, currentDatas: originalDatas})
+                            }
+                        }
+                    })
+                }
+            });
+        })
     }
 
     handleStatusChange(e) {
@@ -42,6 +89,7 @@ export default class App extends Component {
                 return v.status == currentStatus
             }
         })
+
         this.setState({currentStatus: currentStatus, currentDatas: newDatas})
     }
 
@@ -55,7 +103,7 @@ export default class App extends Component {
                     <button className="labelBtn offline" id="offline" onClick={this.handleStatusChange}>offline</button>
                 </div>
                 {this.state.currentDatas.map(function(v){
-                    return <ItemView/>
+                    return <ItemView id={v.name} detail={v.detail} name={v.name} logo={v.logo} nameUrl={v.url} status={v.status}/>
                 })}
             </div>
         );
