6fa8f18857c3466653351a7d58084bbaf470414f
Johnny Gu <johnny@wiredcraft.com>
2017-01-02 19:50:32 +0800

    refactor css

--- src/stories/App.js
+++ src/stories/App.js
@@ -50,9 +50,9 @@ export default class App extends Component {
             <div className="container">
                 <div className="header">
                     <h2 className="title">{title}</h2>
-                    <button className="labelAll selected" id="all" onClick={this.handleStatusChange}>all</button>
-                    <button className="labelOnline" id="online" onClick={this.handleStatusChange}>online</button>
-                    <button className="labelOffline" id="offline" onClick={this.handleStatusChange}>offline</button>
+                    <button className="labelBtn all selected" id="all" onClick={this.handleStatusChange}>all</button>
+                    <button className="labelBtn online" id="online" onClick={this.handleStatusChange}>online</button>
+                    <button className="labelBtn offline" id="offline" onClick={this.handleStatusChange}>offline</button>
                 </div>
                 {this.state.currentDatas.map(function(v){
                     return <ItemView/>
